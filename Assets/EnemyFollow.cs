3b7a9356c5b28cf598d6c6c420a979a91ce8dd76
David <dgb1056@uncw.edu>
2019-03-15 13:59:42 -0400

    Added second island. Added enemy and follow script to enemy. Enemy character now follows the player character.

---
+++ Assets/EnemyFollow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    Transform target; //the enemy's target
+    int moveSpeed = 2; //move speed
+    int rotationSpeed = 2; //speed of turning
+
+    Transform myTransform; //current transform data of this enemy
+
+
+    void Awake()
+    {
+        myTransform = transform; //cache transform data for easy access/preformance
+    }
+
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform; //target the player
+
+
+    }
+
+
+    void Update()
+    {
+        //rotate to look at the player
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+        Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+
+        //move towards the player
+        myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
